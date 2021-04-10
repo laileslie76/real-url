c1e6102513d4cc1d7532b03fa2c8785f704be35f
lppsuixn <lppsuixn@gmail.com>
2021-04-10 16:44:39 +0800

    直播服务器

---
+++ server.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+from huya import HuYa
+from douyu import DouYu
+from youku import YouKu
+class GetHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = parse.urlparse(self.path)
+        print(parsed_path.query)
+        print(parsed_path.path)
+        url = ''
+        if 'huya' in parsed_path.path:
+            h = HuYa(parsed_path.query)
+            url = h.get_real_url()['bd']
+        elif 'douyu' in parsed_path.path:
+            d = DouYu(parsed_path.query)
+            url = d.get_real_url()
+        elif 'youku' in parsed_path.path:
+            y = YouKu(parsed_path.query)
+            url = y.get_real_url()
+        print(url)
+        self.send_response(301)
+        self.send_header('Location',url)
+        self.end_headers()
+
+if __name__ == '__main__':
+    from http.server import HTTPServer
+    server = HTTPServer(('127.0.0.1', 8088), GetHandler)
+    print('Starting server, use <Ctrl-C> to stop')
+    server.serve_forever()
