58004cebd4f61197a16d54a52d9f77c7593d7084
lppsuixn <lppsuixn@gmail.com>
2021-11-14 20:12:48 +0800

    update

--- server-flask.py
+++ server-flask.py
@@ -1,5 +1,4 @@
 from flask import Flask,request, jsonify,redirect,make_response
-from huya import HuYa
 from douyu import DouYu
 from youku import YouKu
 from bs4 import BeautifulSoup
@@ -11,21 +10,8 @@ cache = ExpiringDict(max_len=100, max_age_seconds=300)
 @app.route('/<plat>/<rid>')
 def get_url(plat,rid):
     if 'huya' == plat:
-        h = HuYa(rid)
-        url = h.get_real_url()
-        if rid in cache:
-            cache[rid] +=1 
-        else:
-            cache[rid] = 0
-        item =  cache[rid] % 4
-        if item == 0:
-            url = url['bd_flv']
-        if item == 1:
-            url = url['bd_hls']
-        if item == 2:
-            url = url['al_flv']
-        if item == 3:
-            url = url['al_hls']
+        # h = HuYa(rid)
+        url = get_real_url(rid)
     elif 'douyu'  == plat:
         d = DouYu(rid)
         url = d.get_real_url()
@@ -75,6 +61,57 @@ def get_all_tv():
     response.mimetype = "text/plain"
     return response
 
+
+import requests
+import re
+import base64
+import urllib.parse
+import hashlib
+import time
+
+
+def live(e):
+    i, b = e.split('?')
+    r = i.split('/')
+    s = re.sub(r'.(flv|m3u8)', '', r[-1])
+    c = b.split('&', 3)
+    c = [i for i in c if i != '']
+    n = {i.split('=')[0]: i.split('=')[1] for i in c}
+    fm = urllib.parse.unquote(n['fm'])
+    u = base64.b64decode(fm).decode('utf-8')
+    p = u.split('_')[0]
+    f = str(int(time.time() * 1e7))
+    l = n['wsTime']
+    t = '0'
+    h = '_'.join([p, t, s, f, l])
+    m = hashlib.md5(h.encode('utf-8')).hexdigest()
+    y = c[-1]
+    url = "{}?wsSecret={}&wsTime={}&u={}&seqid={}&{}".format(i, m, l, t, f, y)
+    return url
+
+
+def get_real_url(room_id):
+    try:
+        room_url = 'https://m.huya.com/' + str(room_id)
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/75.0.3770.100 Mobile Safari/537.36 '
+        }
+        response = requests.get(url=room_url, headers=header).text
+        liveLineUrl = re.findall(r'"liveLineUrl":"([\s\S]*?)",', response)[0]
+        liveline = base64.b64decode(liveLineUrl).decode('utf-8')
+        if liveline:
+            if 'replay' in liveline:
+                return '直播录像：' + liveline
+            else:
+                liveline = live(liveline)
+                real_url = ("https:" + liveline).replace("hls", "flv").replace("m3u8", "flv")
+        else:
+            real_url = '未开播或直播间不存在'
+    except:
+        real_url = '未开播或直播间不存在'
+    return real_url
 
 
 if __name__ == '__main__':
